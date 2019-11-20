5840bbbe46e49a09cd5701a95083cf4adaaf28b8
BlaackRabbit <33602013+Potato@users.noreply.github.com>
2019-11-19 22:42:56 -0300

    New colors added

--- lib/utils/colors_palette.dart
+++ lib/utils/colors_palette.dart
@@ -5,6 +5,12 @@ class ColorsPalette {
   /// Primary Color
   static const Color primaryColor = const Color(0XFF34B6DC);
 
+  /// Primary Color Dark
+  static const Color primaryColorDark = const Color(0XFF0085aa);
+
+  /// Primary Color Light
+  static const Color primaryColorLight = const Color(0XFF75e7ff);
+
   /// Accent Color
   static const Color accentColor = const Color(0XFFFF2152);
 
@@ -19,7 +25,9 @@ class ColorsPalette {
 
   /// Background Color Light version
   static const Color backgroundColorLight = const Color(0XFFFAFAFA);
-  
+
+  /// Background Color Light Gray
+  static const Color backgroundColorLightGray = const Color(0XFFE6E6E6);
 }
 
 /// Schema Color URL
