f9af1e1cdc1e6349dc79021a3e7d4780f98b3e0e
BlaackRabbit <33602013+Potato@users.noreply.github.com>
2019-11-19 22:44:26 -0300

    Set custom status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'package:firebase_example/ui/activities/sign_in.dart';
 import 'package:firebase_example/utils/root_activities.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_example/utils/colors_palette.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: ColorsPalette.primaryColorDark, // status bar color
+  ));
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
