d37e50ed4056110447d07e3b103ccc63d3f1aff5
BlaackRabbit <33602013+Potato@users.noreply.github.com>
2019-11-22 23:33:16 -0300

    FirebaseService created

---
+++ lib/database/firebase_service.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_example/models/item.dart';
+import 'package:firebase_example/utils/constants.dart';
+
+/// Stores all the custom colors used in this application
+class FirebaseService {
+  static var _databaseRef = FirebaseDatabase.instance.reference();
+
+  ///
+  static Future<void> create(Item item) async {
+    final _itemRef = _databaseRef.child(FirebaseConstant.item);
+    final _id = _itemRef.push().key;
+
+    _itemRef.child(_id).set(item.toJson());
+  }
+
+  ///
+  static Future<List<Item>> read() async {
+    List<Item> items = [];
+    final _itemRef = _databaseRef.child(FirebaseConstant.item);
+    await _itemRef.once().then((DataSnapshot snapshot) {
+      final value = snapshot.value as Map;
+      for (final key in value.keys) {
+        Item i = Item.fromJson(snapshot.value[key]);
+        items.add(i);
+      }
+      print(items.toString());
+    });
+    return items;
+  }
+
+  ///
+  static Future<void> update(Item item) async {
+    final _itemRef = _databaseRef.child(FirebaseConstant.item);
+    _itemRef.child(item.id).update(item.toJson());
+  }
+
+  ///
+  static Future<void> delete(String id) async {
+    final _itemRef = _databaseRef.child(FirebaseConstant.item);
+    _itemRef.child(id).remove();
+  }
+}
